252841a2c18b5f213f218e1d25b66201f5d9d562
JaretWright <hkkA223>
2018-11-13 09:31:49 -0500

    Updated GUI with buttons

--- src/AboutMeController.java
+++ src/AboutMeController.java
@@ -1,6 +1,8 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -9,12 +11,27 @@ import javafx.fxml.Initializable;
  */
 public class AboutMeController implements Initializable {
 
+    @FXML
+    private Label nameLabel;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+//        nameLabel.setText("I just changed the text");
     }    
+    
+    @FXML
+    public void showNickName()
+    {
+        nameLabel.setText("My friends call me JDub");
+    }
+    
+    @FXML
+    public void showProperName()
+    {
+        nameLabel.setText("My name is Jaret Wright");
+    }
     
 }
